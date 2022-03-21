15aabb34c6581b2a068adca9962e7dfe7cd73f84
MarinaZakharova <marysyazakharova@gmail.com>
2022-03-21 13:35:59 +0300

    revised 4th task 1 mod

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -3,6 +3,3 @@ function checkSpam(str) {
   if (LowerStr.includes('1xbet') || LowerStr.includes('xxx')) return true;
   else return false;
 } 
-alert( checkSpam('1XbeT now') );
-alert( checkSpam('free xXXxx') );
-alert( checkSpam("innocent rabbit") );
